0a52cd0d749effc4b479f59aa3920786b493d65d
mako_kage <makoto120208@gmail.com>
2023-09-12 18:37:46 +0900

    create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/transaction_pool")
+def get_transaction():
+
+    pass
+
+@app.get("/chain")
+def get_chain():
+
+    pass
+
+@app.post("/transaction_pool")
+def post_transaction_pool():
+
+    pass
+
+@app.get("/creat_block")
+def create_block():
+
+    pass
