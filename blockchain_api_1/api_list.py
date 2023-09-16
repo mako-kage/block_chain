54bbab79a0dfb7a8fd06e6c2ecb0945ac341d6db
mako_kage <makoto120208@gmail.com>
2023-09-16 14:10:32 +0900

    changed api_list

--- blockchain_api_1/api_list.py
+++ blockchain_api_1/api_list.py
@@ -1,1 +1,2 @@
-API_LIST = ["http://127.0.0.1:8002", "http://127.0.0.1:8003"]
+# API_LIST = ["http://127.0.0.1:8002", "http://127.0.0.1:8003"]
+API_LIST = ["https://block-chain-api-2.onrender.com", "https://block-chain-api-3.onrender.com"]
--- blockchain_api_2/api_list.py
+++ blockchain_api_2/api_list.py
@@ -1,1 +1,2 @@
-API_LIST = ["http://127.0.0.1:8001", "http://127.0.0.1:8003"]
+# API_LIST = ["http://127.0.0.1:8001", "http://127.0.0.1:8003"]
+API_LIST = ["https://block-chain-p4f6.onrender.com", "https://block-chain-api-3.onrender.com"]
